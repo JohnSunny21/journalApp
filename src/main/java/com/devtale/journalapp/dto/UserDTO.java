93a6cdbc668547348433ba7b883ac9cf6409b1f1
JohnSunny <sunnyjohnballa@gmail.com>
2025-05-09 01:01:53 +0530

    Updating the pom and adding the model

--- src/main/java/com/devtale/journalapp/dto/UserDTO.java
+++ src/main/java/com/devtale/journalapp/dto/UserDTO.java
@@ -2,11 +2,13 @@ package com.devtale.journalapp.dto;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
+
 
 @Data
 @NoArgsConstructor
